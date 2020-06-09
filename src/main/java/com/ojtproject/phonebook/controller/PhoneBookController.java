e9d37ce2d1aec34b4a6cc3b53cb913c649ed2999
shiori_yamagishi <yamagishi@kmkworld.com>
2020-06-09 18:01:39 +0900

    レポジトリ変更後の初期コミット

---
+++ src/main/java/com/ojtproject/phonebook/controller/PhoneBookController.java
@@ -0,0 +1,30 @@
+package com.ojtproject.phonebook.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ojtproject.phonebook.form.SearchForm;
+import com.ojtproject.phonebook.service.SearchService;
+
+
+
+public class PhoneBookController {
+	@Autowired
+	private SearchService search;
+
+	/**トップページを表示*/
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+		return search(new SearchForm(), mav);
+	}
+
+	/**検索ロジックを呼び出して検索ページへ遷移*/
+	@RequestMapping(value = "/search", method = RequestMethod.POST)
+	public ModelAndView search(SearchForm input, ModelAndView mav) {
+		search.execute(input, mav);
+		return mav;
+	}
+
+}
---
+++ src/main/java/com/ojtproject/phonebook/form/SearchForm.java
@@ -0,0 +1,15 @@
+package com.ojtproject.phonebook.form;
+
+public class SearchForm {
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	/**検索キーワード*/
+	private String keyword;
+
+}
