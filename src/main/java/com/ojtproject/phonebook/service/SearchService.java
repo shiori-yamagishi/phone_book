0f18394818ed72fdef8c69ef30f8fe7a0b185ce9
shiori_yamagishi <yamagishi@kmkworld.com>
2020-06-10 14:45:11 +0900

    searchServiceクラスの修正

--- src/main/java/com/ojtproject/phonebook/service/SearchService.java
+++ src/main/java/com/ojtproject/phonebook/service/SearchService.java
@@ -1,6 +1,9 @@
 package com.ojtproject.phonebook.service;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,24 +12,49 @@ import org.springframework.web.servlet.ModelAndView;
 import com.ojtproject.phonebook.dao.PhoneBookRepository;
 import com.ojtproject.phonebook.entity.PhoneBook;
 import com.ojtproject.phonebook.form.SearchForm;
+import com.ojtproject.phonebook.form.SearchResultForm;
 
 @Service
 public class SearchService {
 
 	@Autowired
+	private HttpSession session;
+	@Autowired
+	private PhoneBookRepository phoneBookRepository;
 
-	public PhoneBookRepository phoneBookRepository;
+	/**入力された名前と電話帳リストにある名前を比較して合致するものをListに格納するメソッド*/
+	public void execute(SearchForm input, ModelAndView mav) {
+		List<PhoneBook> phoneBookList = null;
+		String keyword = input.getKeyword();//入力された名前を取得
 
-	public List<PhoneBook> findAll() {
-		return phoneBookRepository.findAll();
+		List<SearchResultForm> searchList = new ArrayList<>();
+		if (keyword == null) {
+			phoneBookList = phoneBookRepository.findAll();
+		} else if (!keyword.equals("")) {
+
+		}
+
+		session.setAttribute("phoneBookList", phoneBookList);
+		for (int i = 0; i < phoneBookList.size(); i++) {
+			PhoneBook entity = phoneBookList.get(i);
+			SearchResultForm sf = new SearchResultForm();
+			sf.setName(entity.getName());
+			sf.setPhoneNumber(entity.getPhoneNumber());
+			searchList.add(sf);
+		}
+		mav.addObject("searchList", searchList);
+		mav.setViewName("search");
+		SearchService.searchMsg(searchList, keyword, mav);
 	}
 
-	/* public void delete(int id) {
-	    phoneBookRepository.deleteById(id);
-	}*/
-
-	public void execute(SearchForm input, ModelAndView mav) {
-		mav.setViewName("search");
-
+	private static void searchMsg(List<SearchResultForm> searchList, String inputName, ModelAndView mav) {
+		/*if (inputName == null) {return;}
+		if (inputName.equals("")) {
+			mav.addObject("msg", Message.SEARCH_EMPTY);
+		} else if (searchList.size() == 0) {
+			mav.addObject("msg", Message.SEARCH_NOT_HIT);
+		} else {
+			mav.addObject("msg", searchList.size() + Message.SEARCH_HIT_COUNT);
+		}*/
 	}
 }
